dc703bedf71a910c683337089724fcac4d8c5c23
Ryan-Brosius <103217571+Ryan-Brosius@users.noreply.github.com>
2024-05-12 12:31:22 -0400

    Player Movement Script

--- player.gd
+++ player.gd
@@ -1,6 +1,32 @@
 extends CharacterBody2D
 
+#Player movement, fish-like movement(?)
+#I would like to make it so character points towards normal vector
+
+const max_speed = 600;
+const accel = 1500;
+const friction = 800;
+
+var input = Vector2.ZERO;
+
 func _physics_process(delta):
-	var direction = Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
-	velocity = direction * 6000 * delta;
-	move_and_slide()
+	player_movement(delta)
+
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"));
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"));
+	return input.normalized();
+	
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * friction * delta
+		else:
+			velocity = Vector2.ZERO;
+	else:
+		velocity += input * accel * delta
+		velocity = velocity.limit_length(max_speed)
+		
+	move_and_slide();
